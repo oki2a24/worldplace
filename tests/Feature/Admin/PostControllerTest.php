d81dbe8356990bb830deda38302111d3f8ab3d65
oki2a24 <oki2a24@gmail.com>
2019-07-31 08:11:18 +0900

    feat: post_statusが適切なレコードが表示されること、というテストを追加する

--- tests/Feature/Admin/PostControllerTest.php
+++ tests/Feature/Admin/PostControllerTest.php
@@ -33,7 +33,6 @@ class PostControllerTest extends TestCase
         $attachment = factory(Post::class, 'post_type_attachment')->create();
         $revision = factory(Post::class, 'post_type_revision')->create();
         $navMenuItem = factory(Post::class, 'post_type_nav_menu_item')->create();
-        dump(Post::all());
 
         $response = $this->get('/admin/posts');
 
@@ -45,5 +44,36 @@ class PostControllerTest extends TestCase
             ->assertDontSee($attachment->post_title)
             ->assertDontSee($revision->post_title)
             ->assertDontSee($navMenuItem->post_title);
+    }
+
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function post_statusが適切なレコードが表示されること()
+    {
+        $publish = factory(Post::class, 'post_type_post')->create();
+        $future = factory(Post::class, 'post_status_future')->create();
+        $draft = factory(Post::class, 'post_status_draft')->create();
+        $pending = factory(Post::class, 'post_status_pending')->create();
+        $private = factory(Post::class, 'post_status_private')->create();
+        $trash = factory(Post::class, 'post_status_trash')->create();
+        $autoDraft = factory(Post::class, 'post_status_auto-draft')->create();
+        $inherit = factory(Post::class, 'post_status_inherit')->create();
+
+        $response = $this->get('/admin/posts');
+
+        $response
+            ->assertViewIs('admin.posts.index')
+            ->assertStatus(200)
+            ->assertSee($publish->post_title)
+            ->assertSee($future->post_title)
+            ->assertSee($draft->post_title)
+            ->assertSee($pending->post_title)
+            ->assertSee($private->post_title)
+            ->assertSee($trash->post_title)
+            ->assertDontSee($autoDraft->post_title)
+            ->assertDontSee($inherit->post_title);
     }
 }
