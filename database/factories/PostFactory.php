8b61b4217a2115a4459df35951d5545304543c96
oki2a24 <oki2a24@gmail.com>
2019-08-06 08:32:54 +0900

    refactor: 繰り返しをまとめる

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -5,7 +5,7 @@
 use App\Models\Wp\Post;
 use Faker\Generator as Faker;
 
-$factory->define(Post::class, function (Faker $faker) {
+$postType = function (Faker $faker, string $postType) {
     return [
         'post_author' => 1,
         'post_date' => $faker->dateTime,
@@ -26,121 +26,33 @@ $factory->define(Post::class, function (Faker $faker) {
         'post_parent' => 0,
         'guid' => $faker->url,
         'menu_order' => 0,
-        'post_type' => 'post',
+        'post_type' => $postType,
         'post_mime_type' => '',
         'comment_count' => 0,
     ];
+};
+
+$factory->define(Post::class, function (Faker $faker) use ($postType) {
+    return $postType($faker, 'post');
 }, 'post_type_post');
 
-$factory->define(Post::class, function (Faker $faker) {
-    return [
-        'post_author' => 1,
-        'post_date' => $faker->dateTime,
-        'post_date_gmt' => $faker->dateTime,
-        'post_content' => $faker->realText,
-        'post_title' => $faker->realText(100),
-        'post_excerpt' => '',
-        'post_status' => 'publish',
-        'comment_status' => 'open',
-        'ping_status' => 'open',
-        'post_password' => '',
-        'post_name' => $faker->text(100),
-        'to_ping' => '',
-        'pinged' => '',
-        'post_modified' => $faker->dateTime,
-        'post_modified_gmt' => $faker->dateTime,
-        'post_content_filtered' => '',
-        'post_parent' => 0,
-        'guid' => $faker->url,
-        'menu_order' => 0,
-        'post_type' => 'page',
-        'post_mime_type' => '',
-        'comment_count' => 0,
-    ];
+$factory->define(Post::class, function (Faker $faker) use ($postType) {
+    return $postType($faker, 'page');
 }, 'post_type_page');
 
-$factory->define(Post::class, function (Faker $faker) {
-    return [
-        'post_author' => 1,
-        'post_date' => $faker->dateTime,
-        'post_date_gmt' => $faker->dateTime,
-        'post_content' => $faker->realText,
-        'post_title' => $faker->realText(100),
-        'post_excerpt' => '',
-        'post_status' => 'publish',
-        'comment_status' => 'open',
-        'ping_status' => 'open',
-        'post_password' => '',
-        'post_name' => $faker->text(100),
-        'to_ping' => '',
-        'pinged' => '',
-        'post_modified' => $faker->dateTime,
-        'post_modified_gmt' => $faker->dateTime,
-        'post_content_filtered' => '',
-        'post_parent' => 0,
-        'guid' => $faker->url,
-        'menu_order' => 0,
-        'post_type' => 'attachment',
-        'post_mime_type' => '',
-        'comment_count' => 0,
-    ];
+$factory->define(Post::class, function (Faker $faker) use ($postType) {
+    return $postType($faker, 'attachment');
 }, 'post_type_attachment');
 
-$factory->define(Post::class, function (Faker $faker) {
-    return [
-        'post_author' => 1,
-        'post_date' => $faker->dateTime,
-        'post_date_gmt' => $faker->dateTime,
-        'post_content' => $faker->realText,
-        'post_title' => $faker->realText(100),
-        'post_excerpt' => '',
-        'post_status' => 'publish',
-        'comment_status' => 'open',
-        'ping_status' => 'open',
-        'post_password' => '',
-        'post_name' => $faker->text(100),
-        'to_ping' => '',
-        'pinged' => '',
-        'post_modified' => $faker->dateTime,
-        'post_modified_gmt' => $faker->dateTime,
-        'post_content_filtered' => '',
-        'post_parent' => 0,
-        'guid' => $faker->url,
-        'menu_order' => 0,
-        'post_type' => 'revision',
-        'post_mime_type' => '',
-        'comment_count' => 0,
-    ];
+$factory->define(Post::class, function (Faker $faker) use ($postType) {
+    return $postType($faker, 'revision');
 }, 'post_type_revision');
 
-$factory->define(Post::class, function (Faker $faker) {
-    return [
-        'post_author' => 1,
-        'post_date' => $faker->dateTime,
-        'post_date_gmt' => $faker->dateTime,
-        'post_content' => $faker->realText,
-        'post_title' => $faker->realText(100),
-        'post_excerpt' => '',
-        'post_status' => 'publish',
-        'comment_status' => 'open',
-        'ping_status' => 'open',
-        'post_password' => '',
-        'post_name' => $faker->text(100),
-        'to_ping' => '',
-        'pinged' => '',
-        'post_modified' => $faker->dateTime,
-        'post_modified_gmt' => $faker->dateTime,
-        'post_content_filtered' => '',
-        'post_parent' => 0,
-        'guid' => $faker->url,
-        'menu_order' => 0,
-        'post_type' => 'nav_menu_item',
-        'post_mime_type' => '',
-        'comment_count' => 0,
-    ];
+$factory->define(Post::class, function (Faker $faker) use ($postType) {
+    return $postType($faker, 'revision');
 }, 'post_type_nav_menu_item');
 
-$factory->define(Post::class, function (Faker $faker) {
+$postStatus = function (Faker $faker, string $postStatus) {
     return [
         'post_author' => 1,
         'post_date' => $faker->dateTime,
@@ -148,7 +60,7 @@ $factory->define(Post::class, function (Faker $faker) {
         'post_content' => $faker->realText,
         'post_title' => $faker->realText(100),
         'post_excerpt' => '',
-        'post_status' => 'future',
+        'post_status' => $postStatus,
         'comment_status' => 'open',
         'ping_status' => 'open',
         'post_password' => '',
@@ -165,166 +77,32 @@ $factory->define(Post::class, function (Faker $faker) {
         'post_mime_type' => '',
         'comment_count' => 0,
     ];
+};
+
+$factory->define(Post::class, function (Faker $faker) use ($postStatus) {
+    return $postStatus($faker, 'future');
 }, 'post_status_future');
 
-$factory->define(Post::class, function (Faker $faker) {
-    return [
-        'post_author' => 1,
-        'post_date' => $faker->dateTime,
-        'post_date_gmt' => $faker->dateTime,
-        'post_content' => $faker->realText,
-        'post_title' => $faker->realText(100),
-        'post_excerpt' => '',
-        'post_status' => 'draft',
-        'comment_status' => 'open',
-        'ping_status' => 'open',
-        'post_password' => '',
-        'post_name' => $faker->text(100),
-        'to_ping' => '',
-        'pinged' => '',
-        'post_modified' => $faker->dateTime,
-        'post_modified_gmt' => $faker->dateTime,
-        'post_content_filtered' => '',
-        'post_parent' => 0,
-        'guid' => $faker->url,
-        'menu_order' => 0,
-        'post_type' => 'post',
-        'post_mime_type' => '',
-        'comment_count' => 0,
-    ];
+$factory->define(Post::class, function (Faker $faker) use ($postStatus) {
+    return $postStatus($faker, 'draft');
 }, 'post_status_draft');
 
-$factory->define(Post::class, function (Faker $faker) {
-    return [
-        'post_author' => 1,
-        'post_date' => $faker->dateTime,
-        'post_date_gmt' => $faker->dateTime,
-        'post_content' => $faker->realText,
-        'post_title' => $faker->realText(100),
-        'post_excerpt' => '',
-        'post_status' => 'pending',
-        'comment_status' => 'open',
-        'ping_status' => 'open',
-        'post_password' => '',
-        'post_name' => $faker->text(100),
-        'to_ping' => '',
-        'pinged' => '',
-        'post_modified' => $faker->dateTime,
-        'post_modified_gmt' => $faker->dateTime,
-        'post_content_filtered' => '',
-        'post_parent' => 0,
-        'guid' => $faker->url,
-        'menu_order' => 0,
-        'post_type' => 'post',
-        'post_mime_type' => '',
-        'comment_count' => 0,
-    ];
+$factory->define(Post::class, function (Faker $faker) use ($postStatus) {
+    return $postStatus($faker, 'pending');
 }, 'post_status_pending');
 
-$factory->define(Post::class, function (Faker $faker) {
-    return [
-        'post_author' => 1,
-        'post_date' => $faker->dateTime,
-        'post_date_gmt' => $faker->dateTime,
-        'post_content' => $faker->realText,
-        'post_title' => $faker->realText(100),
-        'post_excerpt' => '',
-        'post_status' => 'private',
-        'comment_status' => 'open',
-        'ping_status' => 'open',
-        'post_password' => '',
-        'post_name' => $faker->text(100),
-        'to_ping' => '',
-        'pinged' => '',
-        'post_modified' => $faker->dateTime,
-        'post_modified_gmt' => $faker->dateTime,
-        'post_content_filtered' => '',
-        'post_parent' => 0,
-        'guid' => $faker->url,
-        'menu_order' => 0,
-        'post_type' => 'post',
-        'post_mime_type' => '',
-        'comment_count' => 0,
-    ];
+$factory->define(Post::class, function (Faker $faker) use ($postStatus) {
+    return $postStatus($faker, 'private');
 }, 'post_status_private');
 
-$factory->define(Post::class, function (Faker $faker) {
-    return [
-        'post_author' => 1,
-        'post_date' => $faker->dateTime,
-        'post_date_gmt' => $faker->dateTime,
-        'post_content' => $faker->realText,
-        'post_title' => $faker->realText(100),
-        'post_excerpt' => '',
-        'post_status' => 'trash',
-        'comment_status' => 'open',
-        'ping_status' => 'open',
-        'post_password' => '',
-        'post_name' => $faker->text(100),
-        'to_ping' => '',
-        'pinged' => '',
-        'post_modified' => $faker->dateTime,
-        'post_modified_gmt' => $faker->dateTime,
-        'post_content_filtered' => '',
-        'post_parent' => 0,
-        'guid' => $faker->url,
-        'menu_order' => 0,
-        'post_type' => 'post',
-        'post_mime_type' => '',
-        'comment_count' => 0,
-    ];
+$factory->define(Post::class, function (Faker $faker) use ($postStatus) {
+    return $postStatus($faker, 'trash');
 }, 'post_status_trash');
 
-$factory->define(Post::class, function (Faker $faker) {
-    return [
-        'post_author' => 1,
-        'post_date' => $faker->dateTime,
-        'post_date_gmt' => $faker->dateTime,
-        'post_content' => $faker->realText,
-        'post_title' => $faker->realText(100),
-        'post_excerpt' => '',
-        'post_status' => 'auto-draft',
-        'comment_status' => 'open',
-        'ping_status' => 'open',
-        'post_password' => '',
-        'post_name' => $faker->text(100),
-        'to_ping' => '',
-        'pinged' => '',
-        'post_modified' => $faker->dateTime,
-        'post_modified_gmt' => $faker->dateTime,
-        'post_content_filtered' => '',
-        'post_parent' => 0,
-        'guid' => $faker->url,
-        'menu_order' => 0,
-        'post_type' => 'post',
-        'post_mime_type' => '',
-        'comment_count' => 0,
-    ];
+$factory->define(Post::class, function (Faker $faker) use ($postStatus) {
+    return $postStatus($faker, 'auto-draft');
 }, 'post_status_auto-draft');
 
-$factory->define(Post::class, function (Faker $faker) {
-    return [
-        'post_author' => 1,
-        'post_date' => $faker->dateTime,
-        'post_date_gmt' => $faker->dateTime,
-        'post_content' => $faker->realText,
-        'post_title' => $faker->realText(100),
-        'post_excerpt' => '',
-        'post_status' => 'inherit',
-        'comment_status' => 'open',
-        'ping_status' => 'open',
-        'post_password' => '',
-        'post_name' => $faker->text(100),
-        'to_ping' => '',
-        'pinged' => '',
-        'post_modified' => $faker->dateTime,
-        'post_modified_gmt' => $faker->dateTime,
-        'post_content_filtered' => '',
-        'post_parent' => 0,
-        'guid' => $faker->url,
-        'menu_order' => 0,
-        'post_type' => 'post',
-        'post_mime_type' => '',
-        'comment_count' => 0,
-    ];
+$factory->define(Post::class, function (Faker $faker) use ($postStatus) {
+    return $postStatus($faker, 'inherit');
 }, 'post_status_inherit');
